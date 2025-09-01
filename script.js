6791b42cf43a6abf1d9c16c889ab61e06e3fc902
Christopher Carmona <c.carmona0990@gmail.com>
2025-09-01 13:50:51 -0400

    fetch zeldagames so that games are loaded when accessing the page

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 import { main } from "./utils/main.js";
 
+    const zeldagamesAPI_url = 'https://zeldagammes-api.onrender.com/zelda_games';
+    
+    const getZeldaGames = async () => {
+        try {
+            const response = await fetch(zeldagamesAPI_url);
+            const data = await response.json(); 
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        } } 
+    getZeldaGames(); 
     main();
--- utils/main.js
+++ utils/main.js
@@ -4,17 +4,16 @@ import { isLoading, observer } from './utilities.js';
 
 
 export function main() {
-    const header = get('header'); // gets the element with id 'header'
-    const main = get('main'); // gets the element with id 'main'
+    const header = get('header'); 
+    const main = get('main'); 
     isLoading(); // removes loading spinner
     
-    observer.observe(header); // observes the header element for intersection
-    observer.observe(main); // observes the main element for intersection
+    // observes elements for intersection and triggers animation
+    observer.observe(header); 
+    observer.observe(main); 
 
     // dynamically creates a list of projects
     let projectList = projects.map(({ id, name, url, img, description, technologies }) => {
-        
-        //console.log(id, name, url, img, description, technologies);
         
         return `
         <div class="project" id="${id}">
